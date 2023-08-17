24639ca9df78862e8652af16f06dea33c5ad13bd
stefan.ristic <stefanristic123>
2023-08-17 20:47:05 +0200

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 builder.Services.AddDbContext<DataContext>(options => { // Lambda izraz - options je parametar koji proslijedjujem bloku
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -17,13 +18,12 @@ builder.Services.AddDbContext<DataContext>(options => { // Lambda izraz - option
 var app = builder.Build();
  
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()){
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseAuthorization();
 
