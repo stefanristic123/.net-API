a28e311643e79deac164c064f9dc301fed61bacf
stefan.ristic <stefanristic123>
2023-08-18 19:08:56 +0200

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,7 @@
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,9 +14,14 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCors();
 
-builder.Services.AddDbContext<DataContext>(options => { // Lambda izraz - options je parametar koji proslijedjujem bloku
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+builder.Services.AddScoped<ITokenService, TokenService>();
+
+// builder.Services.AddDbContext<DataContext>(options => { // Lambda izraz - options je parametar koji proslijedjujem bloku
+//     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+// });
+
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
  
@@ -24,7 +32,7 @@ if (app.Environment.IsDevelopment()){
 }
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
